e657c422423b8e26f9e5e75be08e9b9e5dcd7ba7
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-10-30 16:18:43 -0400

    add police shooting use case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from use_cases.ai_policy import AIPoliciesSearch
+# from use_cases.ai_policy import AIPoliciesSearch
+from use_cases.police_shootings import PoliceShootingsPoliciesSearch
 
 # Load environment variables
 load_dotenv()
@@ -9,17 +10,23 @@ def main():
     # Path to service account key file (from Google Cloud Console)
     SERVICE_ACCOUNT_FILE = './data_storage/semantic-search-1697418273136-1630a5c96bc7.json'
     
-    SPREADSHEET_ID = '1qWjTUjCMFU_K31xGZqPwNMZQg_HVRsmQq15_2pvvjMA'  
+    # SPREADSHEET_ID = '1qWjTUjCMFU_K31xGZqPwNMZQg_HVRsmQq15_2pvvjMA'  # AI policy
+    SPREADSHEET_ID = '1I4nbVVCLLNJXTOqElUOpAlJs1zkNTkZ0A9mtknF6YoA' # mass shootings
 
     # Initialize the AIPoliciesSearch use case
-    ai_policies_search = AIPoliciesSearch(
+    # ai_policies_search = AIPoliciesSearch(
+    #     json_keyfile_path=SERVICE_ACCOUNT_FILE,
+    #     spreadsheet_id=SPREADSHEET_ID
+    # )
+    police_shootings_policies_search = PoliceShootingsPoliciesSearch(
         json_keyfile_path=SERVICE_ACCOUNT_FILE,
         spreadsheet_id=SPREADSHEET_ID
     )
 
     # Execute the search and save results operation
-    ai_policies_search.execute()
+    # ai_policies_search.execute()
+    police_shootings_policies_search.execute()
 
-# This is the standard boilerplate that calls the main() function.
+
 if __name__ == '__main__':
     main()
---
+++ use_cases/police_shootings.py
@@ -0,0 +1,18 @@
+from data_retrieval.web_search import ArticleFetcher
+from data_storage.google_sheets import GoogleSheetsStorage
+
+class PoliceShootingsPoliciesSearch:
+    def __init__(self, json_keyfile_path, spreadsheet_id):
+        self.article_fetcher = ArticleFetcher()
+        self.google_sheets_storage = GoogleSheetsStorage(json_keyfile_path)
+        self.spreadsheet_id = spreadsheet_id
+
+    def execute(self):
+        query = "\"police shootings\" OR (\"police killed\" AND gun)"
+        articles = self.article_fetcher.fetch_articles(query)
+
+        if articles:
+            values = [["Title", "Snippet", "URL"]] + [[article['title'], article['snippet'], article['link']] for article in articles]
+            self.google_sheets_storage.append_data(self.spreadsheet_id, 'Sheet1!A1', values)
+        else:
+            print("No articles found for the provided query.")
