704a7d953e31c129118c57b49856198a033ab26f
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-11-26 23:44:24 -0500

    add newspaper3k text extraction

--- data_retrieval/mediacloud_data.py
+++ data_retrieval/mediacloud_data.py
@@ -6,6 +6,8 @@ from retrying import retry
 import requests_cache
 from tqdm import tqdm
 import mediacloud.api
+from newspaper import Article
+import unicodedata
 
 # get news domains
 collection_id = "38379429"
@@ -17,13 +19,13 @@ cleaned_domains = [url.rstrip('/') for url in cleaned_domains]
 # print(cleaned_domains)
 
 # Enable requests cache
-# requests_cache.install_cache('article_cache')
+requests_cache.install_cache('article_cache')
 
 # Query parameters
-# query_term = '("police shooting" OR "shot by police" OR "police shot" OR "officer-involved shooting" OR "police-involved shooting" OR "police officer shooting" OR "police shot" OR "officer shot"'
-query_term = '("police shooting" OR "shot by police" OR "police shot" OR "officer-involved shooting" OR "police-involved shooting" OR "police officer shooting" OR "police shot" OR "officer shot" OR "deputy shot" OR "sheriff shot" OR "cop shot")'
+query_term = '("police shooting" OR "shot by police" OR "police shot" OR "officer-involved shooting" OR "police-involved shooting" OR "police officer shooting" OR "police shot" OR "officer shot")'
+# query_term = '("police shooting" OR "shot by police" OR "police shot" OR "officer-involved shooting" OR "police-involved shooting" OR "police officer shooting" OR "police shot" OR "officer shot" OR "deputy shot" OR "sheriff shot" OR "cop shot")'
 # query_term = 'police AND shot'
-start = datetime(2023, 11, 6)
+start = datetime(2023, 11, 6) #11/6 - 11/15
 end = datetime(2023, 11, 15) 
 language = "en"
 
@@ -44,10 +46,14 @@ def get_snippet(url):
     response = requests.get(url)
     response.raise_for_status()  
     snippet = response.json().get('snippet', '')
-    sanitized_snippet = snippet.replace('\n', ' ').replace('\r', '').strip()
-    return sanitized_snippet
+    return snippet
     # return response.json().get('snippet', '')
 
+def sanitize_snippet(snippet):
+    sanitized_snippet = snippet.replace('\n', ' ').replace('\r', '').strip()
+    # Normalize Unicode characters
+    normalized_snippet = unicodedata.normalize('NFKD', sanitized_snippet)
+    return sanitized_snippet
 # Retrieve article URLs and snippets
 articles = []
 for list_of_articles in api.all_articles(query, start, end):
@@ -60,15 +66,37 @@ results = pd.DataFrame(articles).sort_values(by='publication_date', ascending=Fa
 # Apply the get_snippet function to each wayback machine url
 for index, article in tqdm(results.iterrows(), total=results.shape[0]):
     try:
-        article_url = article['article_url']
-        article_snippet = get_snippet(article_url)
+        article_url = article['url']
+        
+        extracted_article = Article(article_url)
+        extracted_article.download()
+        extracted_article.parse()
+        article_snippet = extracted_article.text
+        sanitized_snippet = sanitize_snippet(article_snippet)
         # Encode the snippet in utf-8
-        encoded_snippet = article_snippet.encode('utf-8')
-        # results.at[index, 'snippet'] = article_snippet
-        results.loc[index, 'snippet'] = article_snippet
+        # encoded_snippet = sanitized_snippet.encode('utf-8')
+        results.loc[index, 'snippet'] = sanitized_snippet
+    #     article_url = article['article_url']
+    #     article_snippet = get_snippet(article_url)
+    #     # Encode the snippet in utf-8
+    #     encoded_snippet = article_snippet.encode('utf-8')
+    #     # results.at[index, 'snippet'] = article_snippet
+    #     results.loc[index, 'snippet'] = article_snippet
     except Exception as e:
-        print(f"Error retrieving snippet for {article_url}: {e}")
-        results.at[index, 'snippet'] = None  
+        print(f"Error retrieving snippet for {article_url}: {e}, trying wayback machine url...")
+        try:
+            article_url = article['article_url']
+            article_snippet = get_snippet(article_url)
+            sanitized_snippet = sanitize_snippet(article_snippet)
+            # Encode the snippet in utf-8
+            # encoded_snippet = sanitized_snippet.encode('utf-8')
+            # results.at[index, 'snippet'] = article_snippet
+            results.loc[index, 'snippet'] = sanitized_snippet
+        except Exception as e:
+            print(f"Error retrieving snippet for {article_url}: {e}")
+            results.loc[index, 'snippet'] = None
+        
+        
 
 # results.drop_duplicates(subset=['title'], keep='first', inplace=True)
 
