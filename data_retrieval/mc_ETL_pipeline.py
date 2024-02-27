3f24cdf89dea38161468b79271d5953a2d644746
Karen Wang <46947530+karenswang@users.noreply.github.com>
2024-02-27 13:41:22 -0500

    add evaluation script, improve etl workflow

--- data_retrieval/mc_ETL_pipeline.py
+++ data_retrieval/mc_ETL_pipeline.py
@@ -20,39 +20,18 @@ import os
 from dotenv import load_dotenv
 
 
-# set up weaviate client
-
-# Load environment variables from .env file
 load_dotenv()
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-# OPENAI_APIKEY = os.getenv('OPENAI_API_KEY')
-# client = weaviate.Client(
-#     url = "https://semantic-search-24c7be3q.weaviate.network",
-#     auth_client_secret=weaviate.AuthApiKey(api_key="q2qbDfx6ZL90ltqE8T5IoX3IQfdfvyI47aQg"), 
-#     additional_headers = {
-#         "X-OpenAI-Api-Key": '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'
-#     }
-# )
-
-client = weaviate.connect_to_wcs(
-    cluster_url=os.getenv("weaviate_cluster_url"),
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("weaviate_api_key")),
-    headers={"X-OpenAI-Api": os.getenv("OPENAI_API_KEY")}
+OPENAI_APIKEY = os.getenv('OPENAI_API_KEY')
+client = weaviate.Client(
+    url = "https://semantic-search-24c7be3q.weaviate.network",
+    auth_client_secret=weaviate.AuthApiKey(api_key="q2qbDfx6ZL90ltqE8T5IoX3IQfdfvyI47aQg"), 
+    additional_headers = {
+        "X-OpenAI-Api-Key": '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'
+    }
 )
 
-# if client.schema.exists("Article"):
-#     client.schema.delete_class("Article")
-    
-# for pre-vectorized data
-class_obj = {
-    "class": "Article",
-    "vectorizer": "none",
-    "moduleConfig": {
-        "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
-    }
-}
-
-client.schema.create_class(class_obj)
 
 import mediacloud.api
 collection_id = "38379429"
@@ -103,8 +82,8 @@ query_term = '("police shooting" OR "shot by police" OR "police shot" OR "office
     # "killed by police" OR "killed by officer" OR "killed by deputy" OR "killed by sheriff" OR "killed by cop" OR "killed by trooper")'
 
 # query_term = '("gender" AND "hate crime")'
-start = datetime(2024, 1, 1) 
-end = datetime(2024, 1, 1) 
+start = datetime(2023, 12, 1) 
+end = datetime(2023, 12, 15) 
 language = "en"
 
 # DataFrame to store combined results
@@ -132,7 +111,7 @@ combined_results.sort_values(by='publication_date', ascending=False, inplace=Tru
 print(combined_results.shape)
 combined_results.drop_duplicates(subset=['title'], keep='first', inplace=True)
 print("after dropping duplicates: ", combined_results.shape)
-timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
 combined_results.to_csv(f'./data_storage/{timestamp}_no_snippet.csv', index=False)
 
 for index, article in tqdm(combined_results.iterrows(), total=combined_results.shape[0]):
@@ -148,19 +127,19 @@ combined_results.dropna(subset=['snippet'], inplace=True)
 print("after dropping null snippets: ", combined_results.shape)
 combined_results.reset_index(drop=True, inplace=True)
 combined_results.to_csv(f'./data_storage/{start}.csv', index=False)
-print(f"Data retrieval complete. Results saved to './data_storage/{timestamp}.csv'.")
-# combined_results = pd.read_csv(f'./data_storage/{start}.csv')
+print(f"Data retrieval complete. Results saved to './data_storage/{start}.csv'.")
+
+# use this line and uncomment above if importing existing web search results
+# combined_results = pd.read_csv('./data_storage/web_search_results_Dec.csv')
 
 # pre-vectorize the snippet to avoid using huggingface API which costs money
-# model = SentenceTransformer('sentence-transformers/msmarco-MiniLM-L6-cos-v5')
-model = INSTRUCTOR('hkunlp/instructor-large')
-instruction = "Represent the Science title:"
+# model = SentenceTransformer('sentence-transformers/msmarco-MiniLM-L12-cos-v5')
+model = INSTRUCTOR('hkunlp/instructor-xl')
+instruction = "Represent the news articles about police-involved fatal shooting for question answering:"
+
 print("Max Sequence Length:", model.max_seq_length)
-# model.max_seq_length = 512
+model.max_seq_length = 512
 
-
-model = INSTRUCTOR('hkunlp/instructor-large')
-instruction = "Represent the news articles about police-involved shooting for retrieval:"
 
 
 combined_results['snippet_vector'] = None
@@ -176,6 +155,21 @@ for index, row in tqdm(combined_results.iterrows(), total=combined_results.shape
 
 print(combined_results.shape)
 
+if client.schema.exists("Article"):
+    client.schema.delete_class("Article")
+    
+# for pre-vectorized data
+class_obj = {
+    "class": "Article",
+    "vectorizer": "none",
+    "moduleConfig": {
+        "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
+    }
+}
+
+client.schema.create_class(class_obj)
+
+
 # for pre-vectorized data
 client.batch.configure(batch_size=100)
 with client.batch as batch:
@@ -188,13 +182,21 @@ with client.batch as batch:
         batch.add_data_object(properties, "Article", vector=row["snippet_vector"])
 
 
+
 # graphql (allows to group/merge similar results)
 # query_text = "Find articles about police shooting that resulted in death of the victims. The incident is about police officer,deputy, sheriff, trooper, cop who fatally fired shots and killed someone. The victims are dead."
-query_text = "Recent police shooting incidents leading to fatalities of the victims. The incidents are about police officer, deputy, sheriff, trooper, cop who fatally fired shots and killed someone."
+query_text = """
+Breaking news coverage on recent police shooting incidents leading to fatalities of the victims. The incidents are about police officer, deputy, sheriff, trooper, cop who fired shots and killed someone.
+Do not include aggregated summary, list, or archive of incidents happened in the past. Do not include if it's about a past, not recent incident. Only include if the story mentioned the death of the victim.
+"""
+instruction_prompt = "Represent the news articles for retrieval:"
+
+negative_text = "A list of, archive"
 # query_text = "Find recent hate crime incidents targeting gender or gender identity of the victims."
 
 # query_vector = model.encode(query_text).tolist()
-query_vector = model.encode([[instruction,query_text]]).tolist()
+# negative_vector = model.encode(negative_text).tolist()
+query_vector = model.encode([[instruction_prompt,query_text]]).tolist()
 query_vector = [item for sublist in query_vector for item in sublist]
 
 get_articles_group = f"""
@@ -206,23 +208,13 @@ get_articles_group = f"""
       }},
       group: {{
         type: merge,
-        force: 0.001
+        force: 0
       }},
-      limit: 100
+      limit: 80
     ) {{
       title,
       publication_date,
-      snippet,
-      _additional {{
-        generate(
-          singleResult: {{
-            prompt: \"\"\"Summarize the article: '{'{{snippet}}'}' in one sentence.\"\"\"
-          }}
-        ) {{
-          singleResult,
-          error
-        }}
-      }}
+      snippet
     }}
   }}
 }}
