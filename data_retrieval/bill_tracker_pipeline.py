8cbb34a6794f946a044c8aa2e60f2ceea6083d2d
Karen Wang <46947530+karenswang@users.noreply.github.com>
2024-04-09 12:17:30 -0400

    add legislation pipeline

--- data_retrieval/bill_tracker_pipeline.py
+++ data_retrieval/bill_tracker_pipeline.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import pandas as pd
 import requests
 from tqdm import tqdm
-# import unicodedata
 from sentence_transformers import SentenceTransformer
 from InstructorEmbedding import INSTRUCTOR
 
@@ -28,7 +27,7 @@ client = weaviate.Client(
 )
 
 
-state = "NY"
+# state = "NY"
 # # Configuration for API requests
 # api_url = "https://www.billtrack50.com/bt50api/2.1/json/bills"
 # params = {
@@ -64,79 +63,79 @@ state = "NY"
 # print("Shape of result dataframe:", combined_results.shape)
 # print(combined_results.head())
 # combined_results.to_csv(f'./data_storage/legislation/{state}_billtrack50_results.csv', index=False)
-combined_results = pd.read_csv(f'./data_storage/legislation/{state}_billtrack50_results.csv')
+# combined_results = pd.read_csv(f'./data_storage/legislation/{state}_billtrack50_results.csv')
 
 
 
-#------------------ vectorization ------------------
-# pre-vectorize the snippet to avoid using huggingface API which costs money
+# #------------------ vectorization ------------------
+# # pre-vectorize the snippet to avoid using huggingface API which costs money
 model = SentenceTransformer('sentence-transformers/msmarco-MiniLM-L12-cos-v5')
-# model = INSTRUCTOR('hkunlp/instructor-large')
-# instruction = "Represent the legislation bills for retrieval:"
+# # model = INSTRUCTOR('hkunlp/instructor-large')
+# # instruction = "Represent the legislation bills for retrieval:"
 
-print("Max Sequence Length:", model.max_seq_length)
-model.max_seq_length = 512
+# print("Max Sequence Length:", model.max_seq_length)
+# model.max_seq_length = 512
 
 
 
-# Adjusted to vectorize only specific fields as requested
-combined_results['billName_vector'] = None
-combined_results['summary_vector'] = None
-combined_results['keyWords_vector'] = None
+# # Adjusted to vectorize only specific fields as requested
+# combined_results['billName_vector'] = None
+# combined_results['summary_vector'] = None
+# combined_results['keyWords_vector'] = None
 
-# Assuming you have initialized the model as in the original script
-for index, row in tqdm(combined_results.iterrows(), total=combined_results.shape[0]):
-    # Vectorize the specified fields
-    for field in ['billName', 'summary', 'keyWords']:
-        field_value = row[field]
-        vector = model.encode(field_value).tolist()
-        # vector = model.encode([[instruction, field_value]]).tolist()
-        # vector = [item for sublist in vector for item in sublist]
-        combined_results.at[index, f'{field}_vector'] = vector
+# # Assuming you have initialized the model as in the original script
+# for index, row in tqdm(combined_results.iterrows(), total=combined_results.shape[0]):
+#     # Vectorize the specified fields
+#     for field in ['billName', 'summary', 'keyWords']:
+#         field_value = row[field]
+#         vector = model.encode(field_value).tolist()
+#         # vector = model.encode([[instruction, field_value]]).tolist()
+#         # vector = [item for sublist in vector for item in sublist]
+#         combined_results.at[index, f'{field}_vector'] = vector
 
-print(combined_results.shape)
+# print(combined_results.shape)
 
-if client.schema.exists("Legislation"):
-    client.schema.delete_class("Legislation")
+# if client.schema.exists("Legislation"):
+#     client.schema.delete_class("Legislation")
     
-# for pre-vectorized data
-class_obj = {
-    "class": "Legislation",
-    "vectorizer": "none",
-    "moduleConfig": {
-        "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
-    }
-}
+# # for pre-vectorized data
+# class_obj = {
+#     "class": "Legislation",
+#     "vectorizer": "none",
+#     "moduleConfig": {
+#         "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
+#     }
+# }
 
-client.schema.create_class(class_obj)
+# client.schema.create_class(class_obj)
 
 
-# for pre-vectorized data        
-client.batch.configure(batch_size=100)
-with client.batch as batch:
-    for index, row in combined_results.iterrows():
-        properties = {
-            "billID": row['billID'],
-            "stateBillID": row['stateBillID'],
-            "stateCode": row['stateCode'],            
-            "billName": row['billName'],
-            "summary": row['summary'],
-            "sponsorCount": row['sponsorCount'],
-            "sponsors": row['sponsors'],
-            "subjects": row['subjects'],
-            "keyWords": row['keyWords'],
-            "actions": row['actions'],	
-            "lastAction": row['lastAction'],
-            "actionDate": row['actionDate'],
-            "votes": row['votes'],
-            "billProgress": row['billProgress'],	
-            "officialDocument": row['officialDocument'],
-            "created": row['created']   
-        }
-        # concatenated_vector = row['billName_vector'] + row['summary_vector'] + row['keyWords_vector']
+# # for pre-vectorized data        
+# client.batch.configure(batch_size=100)
+# with client.batch as batch:
+#     for index, row in combined_results.iterrows():
+#         properties = {
+#             "billID": row['billID'],
+#             "stateBillID": row['stateBillID'],
+#             "stateCode": row['stateCode'],            
+#             "billName": row['billName'],
+#             "summary": row['summary'],
+#             "sponsorCount": row['sponsorCount'],
+#             "sponsors": row['sponsors'],
+#             "subjects": row['subjects'],
+#             "keyWords": row['keyWords'],
+#             "actions": row['actions'],	
+#             "lastAction": row['lastAction'],
+#             "actionDate": row['actionDate'],
+#             "votes": row['votes'],
+#             "billProgress": row['billProgress'],	
+#             "officialDocument": row['officialDocument'],
+#             "created": row['created']   
+#         }
+#         # concatenated_vector = row['billName_vector'] + row['summary_vector'] + row['keyWords_vector']
 
-        # Adjust the property names and structure according to your schema requirements
-        batch.add_data_object(properties, "Legislation", vector=row['summary_vector'])
+#         # Adjust the property names and structure according to your schema requirements
+#         batch.add_data_object(properties, "Legislation", vector=row['summary_vector'])
 
 
 query_text = """
@@ -144,9 +143,9 @@ Find bills relating to gender identity, LGBTQ+ groups, trans, nonbinary, gender-
 This could include but not limited to topics on : Sex reassignment, gender reassignment, Biological sex, Natural sex hormones, Sex organs etc.
 
 """
-instruction_prompt = "Represent the legislation bill for retrieval:"
+# instruction_prompt = "Represent the legislation bill for retrieval:"
 
-negative_text = "A list of, archive"
+# negative_text = "A list of, archive"
 # query_text = "Find recent hate crime incidents targeting gender or gender identity of the victims."
 
 query_vector = model.encode(query_text).tolist()
@@ -167,21 +166,9 @@ get_legislation_group = f"""
       limit: 80
     ) {{
       billID,
-        stateBillID,
-        stateCode,
-        billName,
-        summary,
-        sponsorCount,
-        sponsors,
-        subjects,
-        keyWords,
-        actions,
-        lastAction,
-        actionDate,
-        votes,
-        billProgress,
-        officialDocument,
-        created,
+        ShortBillName,
+        statesummary,
+        BillText
     }}
   }}
 }}
